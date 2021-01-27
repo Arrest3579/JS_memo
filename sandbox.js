8db6331d1574a887f920bd6949c340b3a7e724dc
user_00 <mm770912@gmail.com>
2021-01-27 23:35:33 +0800

    紀錄用
    就是個紀錄用的
    沒什麼高端技術
    這樣

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+// java script data types
+// number (1.2.3.4...100...)
+// string (sentence.mail address...)
+// boolean (true/false)
+// null(explicity set a variable with no value )
+// undefined(for variables that have not yet been defined)
+// object(complex dara structures -Arrays, Dates, Literals ,etc)
+// symbol(used with objects)
